f17db634fd5b21c557d6f668253b7413dfe56f32
John Diggins <rohnfiggins@gmail.com>
2023-04-16 22:31:31 -0700

    wk2 react @ lesson 2-10

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -3,7 +3,16 @@ import Ticket from "./Ticket";
 
 function TicketList(){
   return (
-    <Ticket />
+    <React.Fragment>
+    <Ticket 
+    location='3A'
+    names='Thato and Haley'
+    issue='Firebase will not save record!'/> {/*these are all props. They can be passed down to child components.*/}
+    <Ticket
+    location='4B'
+    names='Sleater and Kinney'
+    issue='Prop types are throwing an error.'/>
+    </React.Fragment>
   );
 }
 
