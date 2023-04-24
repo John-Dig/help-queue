ba2009e15c5551a66c1d53374d7f0a0c94176b3f
John Diggins <rohnfiggins@gmail.com>
2023-04-24 11:18:46 -0700

    starting to set up tests

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,28 +1,28 @@
 import React from "react";
 import Ticket from "./Ticket";
-import PropTypes from "prop-types"; //PropTypes is a library to help with type-checking
+import PropTypes from "prop-types";
 
+function TicketList(props){
 
-
-
-      
-function TicketList(props) {
   return (
     <React.Fragment>
-      <hr />
-      {props.ticketList.map((ticket,index) => //loop 
-      <ticket names={ticket.names}
-      location={ticket.location}
-      issue={ticket.issue}
-      key={index} />
+      <hr/>
+      {props.ticketList.map((ticket) =>
+        <Ticket 
+          whenTicketClicked={props.onTicketSelection}
+          names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue}
+          id={ticket.id}
+          key={ticket.id}/>
       )}
     </React.Fragment>
   );
 }
 
-// Add propTypes for ticketList.
 TicketList.propTypes = {
-  ticketList: PropTypes.array
+  ticketList: PropTypes.array,
+  onTicketSelection: PropTypes.func
 };
 
 export default TicketList;
